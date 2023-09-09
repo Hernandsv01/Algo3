c28b3063a2a822053833a80e0e87b90ed314b821
Hernandsv01 <hernanxdsv@gmail.com>
2023-09-09 03:55:59 -0300

    Cambio de nombre al archivo principal del tp

---
+++ src/main/java/org/fiuba/algotres/Juego.java
@@ -0,0 +1,12 @@
+package org.fiuba.algotres;
+
+public class Juego {
+
+    public Juego(){
+        System.out.println("Acá iría nuestro juego... Si tuvieramos uno");
+    }
+
+    public static void main(String[] args) {
+        new Juego();
+    }
+}
