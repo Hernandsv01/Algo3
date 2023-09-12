8425838a65304895b4466e801919fd6de1aa57bd
Hernandsv01 <hernanxdsv@gmail.com>
2023-09-12 00:19:57 -0300

    Agregadas las opciones del jugador y sistema de turnos

--- src/main/java/org/fiuba/algotres/Juego.java
+++ src/main/java/org/fiuba/algotres/Juego.java
@@ -1,5 +1,6 @@
 package org.fiuba.algotres;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.fiuba.algotres.herramientas.EntradaSalida.*;
@@ -10,29 +11,47 @@ public class Juego {
     private Jugador jugador2;
     private int turnoActual;
 
+    private final String[] opciones = {"", "Usar habilidad", "Usar item", "Cambiar pokemon", "Rendirse"};
+
     public Juego(Jugador jugador1, Jugador jugador2){
         this.ganador = 0;
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
-        this.turnoActual = primerTurno();
+        this.turnoActual = jugador1.getPokemonActual().getVelocidad()>=jugador2.getPokemonActual().getVelocidad()?1:2;
     }
 
     public void jugar(){
-        System.out.println("Acá iría nuestro juego... Si tuvieramos uno");
+        int opcionElegida;
+        boolean accionCompletada = true;
 
         while(ganador == 0){
+            // Imprimir información
             limpiarConsola();
             imprimirCampo(this);
-            /*
-            lógica de juego
-                1) Imprimir campo (jugador, nombre del pokemon, vida)
-                2) Imprimir opciones
-                3) reimprimir campo cada vez que se elija algo
-             */
+            System.out.println("Turno: jugador " + turnoActual);
+            for(int i = 1; i < opciones.length; i++){
+                System.out.println("\t" + i + ") " + opciones[i]);
+            }
 
-//            turnoActual = turnoActual==1 ? 2 : 1;
-//            ganador = juegoTerminado();
-            ganador = obtenerOpcionUsuario(3);
+            // Obtener opción de usuario y accionar
+            opcionElegida = obtenerOpcionUsuario(opciones.length);
+            if(opcionElegida == 1){
+                accionCompletada = (turnoActual==1?jugador1:jugador2).elegirHabilidad();
+            }else if(opcionElegida == 2){
+                accionCompletada = (turnoActual==1?jugador1:jugador2).elegirItem();
+            }else if(opcionElegida == 3){
+                accionCompletada = (turnoActual==1?jugador1:jugador2).cambiarPokemonActual();
+            }else{
+                ganador = turnoActual==1?2:1;
+                break;
+            }
+
+            // Si se salió, se repite el bucle con el turno del mismo jugador, sino se pasa al siguiente
+            if(accionCompletada){
+                turnoActual = turnoActual==1 ? 2 : 1;
+            }
+
+            ganador = juegoTerminado();
         }
 
         System.out.println("EL JUGADOR " + ganador + " ES EL GANADOR!!!");
@@ -45,38 +64,15 @@ public class Juego {
     public int juegoTerminado(){
         Optional<Pokemon> res1 = jugador1.getPokemons().stream()
                 .filter(pokemon -> pokemon.getVidaActual() > 0)
-                .findAny();
+                .findFirst();
         if(res1.isEmpty()) return 2;
 
         Optional<Pokemon> res2 = jugador2.getPokemons().stream()
                 .filter(pokemon -> pokemon.getVidaActual() > 0)
-                .findAny();
+                .findFirst();
         if(res2.isEmpty()) return 1;
 
         return 0;
-    }
-
-    /**
-     * Verifica quien comienza jugando basado en la velocidad del pokemon de cada jugador
-     * @return El número del jugador que comienza
-     */
-    public int primerTurno(){
-        return jugador1.getPokemonActual().getVelocidad()>=jugador2.getPokemonActual().getVelocidad()?1:2;
-
-//        int veloz1;
-//        int veloz2;
-//
-//        veloz1 = jugador1.getPokemons().stream()
-//                .map(Pokemon::getVelocidad)
-//                .max(Integer::compare)
-//                .get();
-//
-//        veloz2 = jugador2.getPokemons().stream()
-//                .map(Pokemon::getVelocidad)
-//                .max(Integer::compare)
-//                .get();
-//
-//        return veloz1>=veloz2?1:2;
     }
 
     public static void main(String[] args) {
