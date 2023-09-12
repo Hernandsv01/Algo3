92b9ac706eb52e86ef87e1be20ca1cabe41fcd97
Hernandsv01 <hernanxdsv@gmail.com>
2023-09-11 21:13:15 -0300

    Agregada la funcionalidad de primer turno

--- src/main/java/org/fiuba/algotres/Juego.java
+++ src/main/java/org/fiuba/algotres/Juego.java
@@ -1,7 +1,6 @@
 package org.fiuba.algotres;
 
 import java.util.Optional;
-import java.util.Scanner;
 
 import static org.fiuba.algotres.herramientas.EntradaSalida.*;
 
@@ -9,16 +8,17 @@ public class Juego {
     private int ganador;
     private Jugador jugador1;
     private Jugador jugador2;
+    private int turnoActual;
 
     public Juego(Jugador jugador1, Jugador jugador2){
         this.ganador = 0;
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
+        this.turnoActual = primerTurno();
     }
 
     public void jugar(){
         System.out.println("Acá iría nuestro juego... Si tuvieramos uno");
-        Scanner in = new Scanner(System.in);
 
         while(ganador == 0){
             limpiarConsola();
@@ -30,7 +30,9 @@ public class Juego {
                 3) reimprimir campo cada vez que se elija algo
              */
 
-            ganador = juegoTerminado();
+//            turnoActual = turnoActual==1 ? 2 : 1;
+//            ganador = juegoTerminado();
+            ganador = obtenerOpcionUsuario(3);
         }
 
         System.out.println("EL JUGADOR " + ganador + " ES EL GANADOR!!!");
@@ -52,6 +54,29 @@ public class Juego {
         if(res2.isEmpty()) return 1;
 
         return 0;
+    }
+
+    /**
+     * Verifica quien comienza jugando basado en la velocidad del pokemon de cada jugador
+     * @return El número del jugador que comienza
+     */
+    public int primerTurno(){
+        return jugador1.getPokemonActual().getVelocidad()>=jugador2.getPokemonActual().getVelocidad()?1:2;
+
+//        int veloz1;
+//        int veloz2;
+//
+//        veloz1 = jugador1.getPokemons().stream()
+//                .map(Pokemon::getVelocidad)
+//                .max(Integer::compare)
+//                .get();
+//
+//        veloz2 = jugador2.getPokemons().stream()
+//                .map(Pokemon::getVelocidad)
+//                .max(Integer::compare)
+//                .get();
+//
+//        return veloz1>=veloz2?1:2;
     }
 
     public static void main(String[] args) {
