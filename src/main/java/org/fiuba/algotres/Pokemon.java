2ed96c3e719a8ceaefccbfdd34e8946cd76c1656
Hernandsv01 <hernanxdsv@gmail.com>
2023-09-14 01:52:17 -0300

    Agregadas funcionalidades de Jugador y Pokemon

--- src/main/java/org/fiuba/algotres/Pokemon.java
+++ src/main/java/org/fiuba/algotres/Pokemon.java
@@ -1,4 +1,20 @@
 package org.fiuba.algotres;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Pokemon {
+    private int vidaActual = (int)(Math.random()*100);
+    private final int vidaMaxima = 100;
+    private final int velocidad = (int)(Math.random()*100);
+    private final String nombre = "nombrePokemon";
+
+    @Override
+    public String toString() {
+        return nombre + " (" + vidaActual + "❤\uFE0F/" + vidaMaxima + "❤\uFE0F)";
+    }
+    public String getHealthString() {
+        return "(" + vidaActual + "❤\uFE0F/" + vidaMaxima + "❤\uFE0F)";
+    }
 }
