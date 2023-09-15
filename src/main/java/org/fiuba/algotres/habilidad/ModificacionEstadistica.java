752d3d54ad2f2394ed525468b48284ad348e4055
Hernandsv01 <hernanxdsv@gmail.com>
2023-09-15 04:36:16 -0300

    Creada funcionalidad de habilidades básica

--- src/main/java/org/fiuba/algotres/habilidad/ModificacionEstadistica.java
+++ src/main/java/org/fiuba/algotres/habilidad/ModificacionEstadistica.java
@@ -1,4 +1,14 @@
 package org.fiuba.algotres.habilidad;
 
-public class ModificacionEstadistica {
+import org.fiuba.algotres.Pokemon;
+
+public class ModificacionEstadistica extends Habilidad{
+    public ModificacionEstadistica(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void accionarHabilidad(Pokemon atacante, Pokemon victima) {
+        // Falta implementar
+    }
 }
