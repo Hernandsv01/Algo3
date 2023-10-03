b0311da3c942d09a79d9b04052d6e909f8024732
Hernandsv01 <hernanxdsv@gmail.com>
2023-10-03 15:11:19 -0300

    Divididos los enums a un archivo para cada uno

---
+++ src/main/java/org/fiuba/algotres/TiposBasicos.java
@@ -0,0 +1,19 @@
+package org.fiuba.algotres;
+
+public enum TiposBasicos {
+    AGUA,
+    BICHO,
+    DRAGON,
+    ELECTRICO,
+    FANTASMA,
+    FUEGO,
+    HIELO,
+    LUCHA,
+    NORMAL,
+    PLANTA,
+    PSIQUICO,
+    ROCA,
+    TIERRA,
+    VENENO,
+    VOLADOR
+}
