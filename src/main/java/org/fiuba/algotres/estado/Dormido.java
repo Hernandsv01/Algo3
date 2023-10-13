dbbcdcb341adb3ae744d1d1e6c7373f3d05f1391
Hernandsv01 <hernanxdsv@gmail.com>
2023-10-13 03:43:41 -0300

    Metidos numeros imaginarios en constantes

--- src/main/java/org/fiuba/algotres/estado/Dormido.java
+++ src/main/java/org/fiuba/algotres/estado/Dormido.java
@@ -3,13 +3,16 @@ package org.fiuba.algotres.estado;
 import org.fiuba.algotres.Pokemon;
 
 public class Dormido extends Estado {
+    private final double PROBABILIDAD_INICIAL = 0.25;
+    private final double PROBABILIDAD_AGREGADA_POR_TURNO = 0.25;
+
     public Dormido(String nombre) {
         super(nombre);
     }
 
     @Override
     public boolean accionar(Pokemon pokemon) {
-        double proba = 0.25 + this.turnosAplicados * 0.25;
+        double proba = PROBABILIDAD_INICIAL + this.turnosAplicados * PROBABILIDAD_AGREGADA_POR_TURNO;
         if (Math.random() <= proba) {
             pokemon.setEstado(null);
             return true;
--- src/main/java/org/fiuba/algotres/estado/Paralizado.java
+++ src/main/java/org/fiuba/algotres/estado/Paralizado.java
@@ -3,12 +3,13 @@ package org.fiuba.algotres.estado;
 import org.fiuba.algotres.Pokemon;
 
 public class Paralizado extends Estado {
+    private final double PROBABILIDAD = 0.5;
     public Paralizado(String nombre) {
         super(nombre);
     }
 
     @Override
     public boolean accionar(Pokemon pokemon) {
-        return Math.random() >= 0.5;
+        return Math.random() <= PROBABILIDAD;
     }
 }
