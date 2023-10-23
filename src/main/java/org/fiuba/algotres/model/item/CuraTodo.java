15690958dbc7b3e61971c26502a2aedb73fbf172
BlasChuc <bchuc@fi.uba.ar>
2023-10-22 21:00:29 -0300

    Modificacion en CuraTodo, por tener List<Estado>

--- src/main/java/org/fiuba/algotres/model/item/CuraTodo.java
+++ src/main/java/org/fiuba/algotres/model/item/CuraTodo.java
@@ -1,6 +1,7 @@
 package org.fiuba.algotres.model.item;
 
 import org.fiuba.algotres.model.Pokemon;
+import org.fiuba.algotres.model.estado.Estado;
 
 public class CuraTodo extends Item{
     public CuraTodo(int cantidad, String nombre) {
@@ -10,8 +11,8 @@ public class CuraTodo extends Item{
     @Override
     public boolean usar(Pokemon pokemon) {
         if (super.usar(pokemon)) {
-            if (pokemon.getEstado() != null) {
-                pokemon.setEstado(null);
+            if (!pokemon.getEstados().isEmpty()) {
+                pokemon.getEstados().removeAll(pokemon.getEstados());
                 cantidad--;
                 return true;
             }
