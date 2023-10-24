a5c495736f2d6271140f5cd110eea08af0cb12a2
Hernandsv01 <hernanxdsv@gmail.com>
2023-10-24 02:58:25 -0300

    Setup de tests del proyecto

---
+++ src/test/java/org/fiuba/algotres/model/CampoDeBatallaTest.java
@@ -0,0 +1,28 @@
+package org.fiuba.algotres.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class CampoDeBatallaTest {
+
+    @Test
+    public void getGanador() {
+    }
+
+    @Test
+    public void setJugadorInicialPorAtributo() {
+    }
+
+    @Test
+    public void getSiguienteTurno() {
+    }
+
+    @Test
+    public void setSiguienteTurno() {
+    }
+
+    @Test
+    public void getJugadorActual() {
+    }
+}
---
+++ src/test/java/org/fiuba/algotres/model/item/RevivirTest.java
@@ -0,0 +1,12 @@
+package org.fiuba.algotres.model.item;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RevivirTest {
+
+    @Test
+    void usar() {
+    }
+}
