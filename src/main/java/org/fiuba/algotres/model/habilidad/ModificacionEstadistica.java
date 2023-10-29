a21b4a87eeaba77e864b271543f4c84b8aad15ed
BlasChuc <bchuc@fi.uba.ar>
2023-10-29 12:12:39 -0300

    Clases generales que aplican el Strategy.

--- src/main/java/org/fiuba/algotres/model/habilidad/ModificacionEstadistica.java
+++ src/main/java/org/fiuba/algotres/model/habilidad/ModificacionEstadistica.java
@@ -1,13 +1,16 @@
 package org.fiuba.algotres.model.habilidad;
 
 import org.fiuba.algotres.model.Pokemon;
+import org.fiuba.algotres.model.strategy.Strategy;
 
-public abstract class ModificacionEstadistica extends Habilidad{
+public class ModificacionEstadistica extends Habilidad {
+    private final Strategy strategy;
     protected final Integer porcentaje;
 
-    public ModificacionEstadistica(String nombre, int usos, Integer porcentaje) {
+    public ModificacionEstadistica(String nombre, int usos, Integer porcentaje, Strategy strategy) {
         super(nombre, usos);
         this.porcentaje = porcentaje;
+        this.strategy = strategy;
     }
 
     /**
@@ -15,5 +18,16 @@ public abstract class ModificacionEstadistica extends Habilidad{
      * @param victima pokemon contrario.
      */
     @Override
-    public abstract boolean accionarHabilidad(Pokemon atacante, Pokemon victima);
+    public boolean accionarHabilidad(Pokemon atacante, Pokemon victima) {
+        if (verificarUsos(usos)) {
+            if (atacante == victima) {
+                this.strategy.modificar(victima, porcentaje);
+            } else {
+                this.strategy.modificar(victima, -porcentaje);
+            }
+            usos--;
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/org/fiuba/algotres/model/item/Estadistica.java
+++ src/main/java/org/fiuba/algotres/model/item/Estadistica.java
@@ -1,10 +1,25 @@
 package org.fiuba.algotres.model.item;
 
-public abstract class Estadistica extends Item {
+import org.fiuba.algotres.model.Pokemon;
+import org.fiuba.algotres.model.strategy.Strategy;
+
+public class Estadistica extends Item {
+    private final Strategy strategy;
     protected int porcentaje;
 
-    public Estadistica(int cantidad, String nombre, int porcentaje) {
+    public Estadistica(int cantidad, String nombre, int porcentaje, Strategy strategy) {
         super(cantidad, nombre);
         this.porcentaje = porcentaje;
+        this.strategy = strategy;
+    }
+
+    @Override
+    public boolean usar(Pokemon pokemon) {
+        if (verificarCantidad(cantidad)) {
+            this.strategy.modificar(pokemon,porcentaje);
+            cantidad--;
+            return true;
+        }
+        return false;
     }
 }
