21047ebd6b4d0b78f858c96a6590ff6bde44d0a1
BlasChuc <bchuc@fi.uba.ar>
2023-10-29 12:13:06 -0300

    Correccion funcionalidad de "Confuso".

--- src/main/java/org/fiuba/algotres/model/estado/Confuso.java
+++ src/main/java/org/fiuba/algotres/model/estado/Confuso.java
@@ -3,6 +3,7 @@ package org.fiuba.algotres.model.estado;
 import org.fiuba.algotres.model.Pokemon;
 
 public class Confuso extends Estado {
+    private final int DANO = 5;
     private final double PROBABILIDAD = 0.33;
 
     public Confuso(String nombre) {
@@ -13,9 +14,11 @@ public class Confuso extends Estado {
     public boolean accionar(Pokemon pokemon) {
         if (this.turnosAplicados >= 3) {
             pokemon.quitarEstado(this);
-            return true;
         }
         this.turnosAplicados++;
-        return (Math.random() >= PROBABILIDAD);
+        if (Math.random() <= PROBABILIDAD) {
+            pokemon.danarPorPorcentaje(DANO);
+        }
+        return true;
     }
 }
