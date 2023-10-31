38216470da8b1e60a2cdb3efca7d4cd24f09fa49
fource <kevincre20@gmail.com>
2023-10-31 01:22:55 -0300

    Terminado el test de CambiarClima

--- src/test/java/org/fiuba/algotres/model/habilidad/CambiarClimaTest.java
+++ src/test/java/org/fiuba/algotres/model/habilidad/CambiarClimaTest.java
@@ -1,10 +1,10 @@
 package org.fiuba.algotres.model.habilidad;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 import org.fiuba.algotres.model.CampoDeBatalla;
+import org.fiuba.algotres.model.clima.Niebla;
 import org.fiuba.algotres.model.clima.Soleado;
 import org.junit.jupiter.api.Test;
 
@@ -16,23 +16,55 @@ public class CambiarClimaTest {
         Soleado soleado = mock(Soleado.class);
         when(soleado.getCdb()).thenReturn(cdb);
         when(cdb.getClima()).thenReturn(soleado);
-        CambiarClima cambiarClima = new CambiarClima(
-                "Cambiar Clima",
+        CambiarClima cambiarClimaSoleado = new CambiarClima(
+                "Cambiar a soleado",
                 5,
-                    soleado
+                soleado
                 );
 
-        int usosActuales = 5;
+        Niebla niebla = mock(Niebla.class);
+        when(niebla.getCdb()).thenReturn(cdb);
+        CambiarClima cambiarClimaNeblina = new CambiarClima(
+                "Cambiar a neblina",
+                5,
+                niebla
+        );
+
+        int usosActualesSoleado = 5;
+        int usosActualesNiebla = 5;
+        int usosMaximo = 5;
 
         verify(soleado, times(0)).getCdb();
         verify(cdb, times(0)).setClima(soleado);
-        assertEquals(usosActuales, cambiarClima.getUsos());
+        verify(soleado, times(0)).setTurnosAplicados(0);
+        assertEquals(usosActualesSoleado, cambiarClimaSoleado.getUsos());
 
-        cambiarClima.accionarHabilidad(null, null);
-        usosActuales--;
+        cambiarClimaSoleado.accionarHabilidad(null, null);
+        usosActualesSoleado--;
 
         verify(soleado, times(1)).getCdb();
         verify(cdb, times(1)).setClima(soleado);
-        assertEquals(usosActuales, cambiarClima.getUsos());
+        verify(soleado, times(1)).setTurnosAplicados(0);
+        assertEquals(usosActualesSoleado, cambiarClimaSoleado.getUsos());
+
+        for (int i = 1; i < usosMaximo; i++) {
+            verify(niebla, times(i-1)).setTurnosAplicados(0);
+            cambiarClimaNeblina.accionarHabilidad(null, null);
+            usosActualesNiebla--;
+            verify(niebla, times(i)).getCdb();
+            verify(cdb, times(i)).setClima(niebla);
+            assertEquals(usosActualesNiebla, cambiarClimaNeblina.getUsos());
+        }
+        verify(niebla, times(usosMaximo-1)).setTurnosAplicados(0);
+
+
+        cambiarClimaSoleado.accionarHabilidad(null, null);
+        usosActualesSoleado--;
+
+        verify(soleado, times(2)).setTurnosAplicados(0);
+        verify(soleado, times(2)).getCdb();
+        verify(cdb, times(2)).setClima(soleado);
+        assertEquals(usosActualesSoleado, cambiarClimaSoleado.getUsos());
+
     }
 }
