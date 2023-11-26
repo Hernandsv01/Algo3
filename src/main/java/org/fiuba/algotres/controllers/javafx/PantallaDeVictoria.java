3e00f0706d9af06bf51354ac503ecf5824b249ac
tinchosimaj <tsimajowich@gmail.com>
2023-11-26 17:14:30 -0300

    pantalla de victoria

---
+++ src/main/java/org/fiuba/algotres/controllers/javafx/PantallaDeVictoria.java
@@ -0,0 +1,118 @@
+package org.fiuba.algotres.controllers.javafx;
+
+import org.fiuba.algotres.utils.enums.Sound;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class PantallaDeVictoria extends Application {
+    
+    @FXML
+    private Label mensajeVictoria;
+
+    
+
+    private final Sound backgroundMusic = new Sound("/audios/sonidoVictoria.wav");
+
+    @Override
+    public void start(Stage stage ) {
+        try {
+            stage.setTitle("Pantalla de Victoria");
+            stage.setResizable(false);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/pantallaVictoria.fxml"));
+            AnchorPane root = loader.load();
+            ImageView imagenVictoria = setearPantallaVitcoria();
+            ImageView gifEntrenador = setearEntrenadorPokemon();
+            ImageView gifPokemon = setearGifPokemon();
+            //ImageView gifFirework = setearGifFirework();
+            root.getChildren().add(imagenVictoria);
+            root.getChildren().add(gifEntrenador);
+            root.getChildren().add(gifPokemon);
+            //root.getChildren().add(gifFirework);
+            imagenVictoria.toBack();
+            stage.getIcons().add(new Image("/imagenes/otros/app-logo.png"));
+            //playSound();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            
+
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+    
+    
+    public ImageView setearPantallaVitcoria(){
+        Image imagen = new Image(getClass().getResourceAsStream("/imagenes/otros/fondoPantallaVictoria.png"));
+        ImageView imagenVictoria = new ImageView();
+        imagenVictoria.setImage(imagen);
+        
+        return imagenVictoria;
+    }
+
+    public ImageView setearEntrenadorPokemon(){
+        Image gif = new Image(getClass().getResourceAsStream("/imagenes/otros/entrenador.gif"));
+        ImageView gifEntrenador = new ImageView();
+        gifEntrenador.setImage(gif);
+        gifEntrenador.setScaleY(4.5);
+        gifEntrenador.setScaleX(4.5);
+        gifEntrenador.setX(530);
+        gifEntrenador.setY(260);
+        return gifEntrenador;
+
+    }
+
+  // public ImageView setearGifFirework(){
+  //     Image gif = new Image(getClass().getResourceAsStream("/imagenes/otros/fireworks.gif"));
+  //     ImageView gifFirework = new ImageView();
+  //     gifFirework.setImage(gif);
+  //     gifFirework.setScaleY(-0.5);
+  //     gifFirework.setX(508);
+  //     gifFirework.setY(0);
+  //     gifFirework.setScaleX(1.0);
+  //     return gifFirework;
+
+  // }
+
+    public ImageView setearGifPokemon(){
+        //Que tome el pokemon del jugador ganador
+        Image gif = new Image(getClass().getResourceAsStream("/imagenes/pokemons/Paras-front.gif"));
+        //Image gif = new Image(getClass().getResourceAsStream("/imagenes/pokemons/"+ capitalizar(JavafxController.getCdb().getJugadorActual().getPokemonActual().getNombre()) +"-front.gif"));
+        ImageView gifPokemon = new ImageView();
+        gifPokemon.setImage(gif);
+        gifPokemon.setY(240);
+        gifPokemon.setX(50);
+        gifPokemon.setScaleY(2.5);
+        gifPokemon.setScaleX(1.5);
+        return gifPokemon;
+
+    }
+
+    private static String capitalizar(String string){
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
+    }
+
+
+   //public void setMensajeVictoria(){
+   //    mensajeVictoria.setText("Felicidades " + JavafxController.getCdb().getJugadorActual().getNombre() + " has ganado la partida");
+   //}
+
+    public void playSound(){
+        backgroundMusic.playSound(false, -10.0f);
+    }
+
+    public void stopSound(){
+        backgroundMusic.stopSound();
+    }
+}
---
+++ src/main/java/org/fiuba/algotres/utils/enums/Sound.java
@@ -0,0 +1,40 @@
+package org.fiuba.algotres.utils.enums;
+
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Sound {
+
+    private AudioInputStream audioInputStream;
+    private Clip clip;
+    private final String audioFilePath;
+
+    public Sound(String audioFilePath){
+        this.audioFilePath = audioFilePath;
+    }
+
+    public void playSound(boolean isLoop, float decibels){
+        try {
+            audioInputStream = AudioSystem.getAudioInputStream(new File(audioFilePath).getAbsoluteFile());
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+
+            FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volume.setValue(decibels);
+
+            if(isLoop){
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+            clip.start();
+
+        }catch (LineUnavailableException | IOException | UnsupportedAudioFileException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void stopSound(){
+        clip.close();
+    }
+}
